92d9abd2b319197e03e71394429c2b1d21f7360e
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-07 03:20:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,43 @@
 class Transfer
   # your code here
+  attr_accessor :status, :amount
+  attr_reader :receiver, :sender
+
+  @@transaction_hist = []
+
+  def intialize(sender, amount, reciever)
+      @sender = sender
+      @amount = amount
+      @receiver = receiver
+      @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && reciever.valid?
+  end
+
+  def execute_transaction
+    sender.balance -= @amount
+  end
+
+  if valid?
+    @@transaction_hist << amount
+    reciever.deposit(@amount)
+    @amount = 0
+    @status = "complete"
+  else
+    sender.balance += @amount
+    @status = "rejected"
+    "Transaction rejected. Please check your account balance"
+  end
+end
+
+    def reverse_transfer
+      if status == "complete"
+      @sender.deposit(@@transaction_hist.last)  
+      @@transaction_hist.delete(@@transaction_hist.last)
+      @receiver.balance -= @@transaction_hist.last
+      @status = "reversed"
+    end 
+  end    
 end
